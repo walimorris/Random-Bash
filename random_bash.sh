2ab508fde71a98c8568e722bf673b98cdc6b4c9d
walimorris <48896622+walimorris@users.noreply.github.com>
2019-12-24 00:35:38 -0800

    Update random_bash.sh

--- random_bash.sh
+++ random_bash.sh
@@ -18,7 +18,6 @@ fi
 
 echo "How old are you? "
 read -r age 
-
 if [ $age -lt 20 ]; then
     echo "you're a baby!"
 fi
@@ -29,30 +28,24 @@ while [ $age -lt 20 ]; do
     echo "you're now: $age"
 done
 echo "My my... how the years fly!"
-
 #A bash function to take a nap
 a="1m"
 echo "uh-oh, what does $a mean?"
-
 sleepy () {
    echo "Time for a nap"
    sleep $1
    echo "Ahh, I'm rested, now where were we..."
 }
-
 sleepy $a
 
 x=1
 y=2
 let z="1-2"
-
 echo "What number am I: $z"
-
 echo -n "enter name: " ; read name
 echo "$name"
 
 echo "Goodbye, $name"
 cd -- 
 pwd 
-
 clear 
