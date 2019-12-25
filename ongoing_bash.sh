d0f4e451eb16f87ad65c332127da43bd7d0eacdf
walimorris <48896622+walimorris@users.noreply.github.com>
2019-12-25 04:26:05 -0800

    Update ongoing_bash.sh

--- ongoing_bash.sh
+++ ongoing_bash.sh
@@ -43,9 +43,35 @@ x=1
 y=2
 let z="1-2"
 echo "What number am I: $z"
+
+NOW=$(date)
+LOCATION=$(pwd)
+DIR=($cat ls)
+echo "I'm currently in: $LOCATION"
+echo "Currently, it is: $NOW"
+$DIR
+echo "HOME DIRECTORY FILES MONITORED ON: $NOW" > homedir_monitor.txt
+echo >> homedir_monitor.txt
+$DIR >> homedir_monitor.txt
+echo 
+echo "End --------------------------------------------" >> homedir_monitor.txt
+echo "We just created a file would you like to view? [Y/n] " 
+read -r homedir
+if [ "$homedir" = "Y" ] || [ "$homedir" = "y" ]; then
+    cat homedir_monitor.txt
+    else
+        echo "You didn't chose yes, default is not to view, continuing..."
+fi
+echo "Should we keep file? [Y/n] "
+read -r answer
+if [ "$answer" = "Y" ] || [ "$answer" = "y" ]; then
+    echo "File stored"
+    else
+        rm -r homedir_monitor.txt
+fi
+
 echo -n "enter name: " ; read name
 echo "$name"
-
 echo "Goodbye, $name"
 cd -- 
 pwd 
