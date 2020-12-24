5153d78eb7c7ce553ba18b231c1a8d857f1efdf5
Wali Morris <48896622+walimorris@users.noreply.github.com>
2020-12-23 20:20:38 -0800

    Add files via upload

--- compile11.sh
+++ compile11.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
-# A shortcut to compile c++11 files with <filename>-a appended to the end
+# A shortcut to compile c++11 files with <filename>-a appended to the end.
+# No error messages indicate an error-free compile for c++11 program file.
+# Feel free to change the compiled file format to whatever suits you. 
 # Wali Morris 
 
-echo type the name of your program with .cpp appended to the end 
+echo input the name of your program with .cpp appended to the end 
 read program 
-echo compiling $program for cpp11
+echo compiling $program directed-for cpp11
 g++ $program -o $program-a -std=c++11
